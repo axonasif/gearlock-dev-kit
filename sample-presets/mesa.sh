d958f1f17e143a55055d8a310ea2de02242a6d2a
AXIM0S <aminurfisa3@gmail.com>
2020-03-30 16:29:41 +0600

    Simplified mesa sample-script

--- sample-presets/mesa.sh
+++ sample-presets/mesa.sh
@@ -6,66 +6,66 @@
 get_base_dir # Returns the directory path in $BD variable from where this *install.sh is being executed
 #####--- Import Functions ---#####
 
-geco "\n${GREEN}Cleaning up existing mesa deps and dri${RC}"
-
-# nout is used to silence the output
-
+clean_job(){
 #llvm
-nout rm -r /system/lib/libLLVM*
-nout rm -r /system/lib64/libLLVM*
+rm -r /system/lib/libLLVM*
+rm -r /system/lib64/libLLVM*
 #dri
-nout rm -r /system/lib/dri
-nout rm -r /system/lib64/dri
-nout rm -r /system/vendor/lib/dri
-nout rm -r /system/vendor/lib64/dri
-nout rm -r /system/phoenixos_darkmatter/dri
-nout rm -r /system/phoenixos_darkmatter64/dri
+rm -r /system/lib/dri
+rm -r /system/lib64/dri
+rm -r /system/vendor/lib/dri
+rm -r /system/vendor/lib64/dri
+rm -r /system/phoenixos_darkmatter/dri
+rm -r /system/phoenixos_darkmatter64/dri
 #hw
-nout rm -r /system/lib/hw/*
-nout rm -r /system/lib64/hw/*
-nout rm -r /system/vendor/lib/hw/*
-nout rm -r /system/vendor/lib64/hw/*
-#dnout rm
-nout rm -r /system/lib/libdnout rm_*
-nout rm -r /system/lib/libdnout rm.so
-nout rm -r /system/lib64/libdnout rm_*
-nout rm -r /system/lib64/libdnout rm.so
-nout rm -r /system/vendor/lib/libdnout rm_*
-nout rm -r /system/vendor/lib/libdnout rm.so
-nout rm -r /system/vendor/lib64/libdnout rm_*
-nout rm -r /system/vendor/lib64/libdnout rm.so
-#gralloc_dnout rm
-nout rm -r /system/lib/libgralloc_dnout rm.so
-nout rm -r /system/lib64/libgralloc_dnout rm.so
-nout rm -r /system/vendor/lib/libgralloc_dnout rm.so
-nout rm -r /system/vendor/lib64/libgralloc_dnout rm.so
+rm -r /system/lib/hw/*
+rm -r /system/lib64/hw/*
+rm -r /system/vendor/lib/hw/*
+rm -r /system/vendor/lib64/hw/*
+#drm
+rm -r /system/lib/libdrm_*
+rm -r /system/lib/libdrm.so
+rm -r /system/lib64/libdrm_*
+rm -r /system/lib64/libdrm.so
+rm -r /system/vendor/lib/libdrm_*
+rm -r /system/vendor/lib/libdrm.so
+rm -r /system/vendor/lib64/libdrm_*
+rm -r /system/vendor/lib64/libdrm.so
+#gralloc_drm
+rm -r /system/lib/libgralloc_drm.so
+rm -r /system/lib64/libgralloc_drm.so
+rm -r /system/vendor/lib/libgralloc_drm.so
+rm -r /system/vendor/lib64/libgralloc_drm.so
 #gbm
-nout rm -r /system/lib/libgbm.so
-nout rm -r /system/lib64/libgbm.so
-nout rm -r /system/vendor/lib/libgbm.so
-nout rm -r /system/vendor/lib64/libgbm.so
+rm -r /system/lib/libgbm.so
+rm -r /system/lib64/libgbm.so
+rm -r /system/vendor/lib/libgbm.so
+rm -r /system/vendor/lib64/libgbm.so
 #glapi
-nout rm -r /system/lib/libglapi.so
-nout rm -r /system/lib64/libglapi.so
-nout rm -r /system/vendor/lib/libglapi.so
-nout rm -r /system/vendor/lib64/libglapi.so
+rm -r /system/lib/libglapi.so
+rm -r /system/lib64/libglapi.so
+rm -r /system/vendor/lib/libglapi.so
+rm -r /system/vendor/lib64/libglapi.so
 #libGLESmesa
-nout rm -r /system/lib/egl/libGLES_mesa.so
-nout rm -r /system/lib64/egl/libGLES_mesa.so
-nout rm -r /system/vendor/lib/egl/libGLES_mesa.so
-nout rm -r /system/vendor/lib64/egl/libGLES_mesa.so
+rm -r /system/lib/egl/libGLES_mesa.so
+rm -r /system/lib64/egl/libGLES_mesa.so
+rm -r /system/vendor/lib/egl/libGLES_mesa.so
+rm -r /system/vendor/lib64/egl/libGLES_mesa.so
 #GLES-CM_EGL-orig
-nout rm -r /system/lib/libEGL_orig.so
-nout rm -r /system/lib/libGLESv1_CM_orig.so
-nout rm -r /system/lib/libGLESv2_orig.so
-nout rm -r /system/lib/libGLESv3_orig.so
-nout rm -r /system/lib64/libEGL_orig.so
-nout rm -r /system/lib64/libGLESv1_CM_orig.so
-nout rm -r /system/lib64/libGLESv2_orig.so
-nout rm -r /system/lib64/libGLESv3_orig.so
+rm -r /system/lib/libEGL_orig.so
+rm -r /system/lib/libGLESv1_CM_orig.so
+rm -r /system/lib/libGLESv2_orig.so
+rm -r /system/lib/libGLESv3_orig.so
+rm -r /system/lib64/libEGL_orig.so
+rm -r /system/lib64/libGLESv1_CM_orig.so
+rm -r /system/lib64/libGLESv2_orig.so
+rm -r /system/lib64/libGLESv3_orig.so
+}
+
+geco "\n${GREEN}Cleaning up existing mesa deps and dri${RC}"; nout clean_job
 
 geco "\n+ Merging mesa dri & dependencie files in your operating-system"
 gclone "$BD/system" / # You must use quotes " " if any of your file-name contains *spaces or special characters
 
 
-## There are many more awesome functions that you can use, visit https://supreme-gamers.com/gearlock for more infonout rmation.
+## There are many more awesome functions that you can use, visit https://supreme-gamers.com/gearlock for more information.
